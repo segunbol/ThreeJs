107dfb55ab2eb4d2a8d11146c223a927708684f7
segunbol <segunbolawole@yahoo.com>
2022-12-13 12:38:27 +0100

    first commit

---
+++ threejs/main.js
@@ -0,0 +1,59 @@
+import * as THREE from 'three'
+import "./style.css"
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+//Scene
+const scene = new THREE.Scene();
+
+//Create our sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({color:"#00ff83",})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//Sizes
+const sizes = {
+  width:window.innerWidth,
+  height:window.innerHeight
+
+}
+//light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(10, 10, 10)
+scene.add(light)
+
+//Camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100)
+camera.position.z = 10
+scene.add(camera)
+
+//Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGL1Renderer({ canvas })
+renderer.setSize(sizes.width , sizes.height)
+renderer.setPixelRatio(2)
+renderer.render(scene, camera)
+
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
+//Resize
+window.addEventListener("resize", () => {
+  //update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  //update camera
+  camera.updateProjectionMatrix()
+  camera.aspect = sizes.width/ sizes.height
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = () => {
+  controls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+}
+loop()
